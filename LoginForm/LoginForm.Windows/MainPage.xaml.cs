7d4eaee3004f7d94768f62c7767424cecd1435bf
asp <asp@LAB20-57>
2019-06-10 18:16:12 +0500

    Add project files.

---
+++ LoginForm/LoginForm.Windows/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LoginForm
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(User.Text=="admin"&&Pass.Text=="admin")
+            {
+                this.Frame.Navigate(typeof(BasicPage1), null);
+            }
+            else
+            {
+                MessageDialog msg = new MessageDialog("Invaild Username Or Password");
+                await msg.ShowAsync();
+
+            }
+        }
+    }
+}
